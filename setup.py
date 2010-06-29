a1a49939afef2c0f619c3082a2cbdc8f9e1ff8a1
pc <pc@513fb99f-98dc-411c-91f8-8cf53895d036>
2010-06-29 23:21:53 +0000

    `setup.py` file
    git-svn-id: svn://svn.gafol.net/cryp/branches/dbus@8 513fb99f-98dc-411c-91f8-8cf53895d036

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Cryp',
+      version='0.2',
+      description='Simple password management application',
+      author='Paul Colomiets',
+      author_email='pc@gafol.net',
+      url='svn://svn.gafol.net/cryp/',
+      packages=['cryp'],
+     )
